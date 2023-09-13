09cf7599fee91d487d4a922a496e6809abc29cc8
LittleAdventurer <pyne469@gmail.com>
2023-09-13 18:26:11 +0800

    Adding Location Feature

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController, UITextFieldDelegate {
+class WeatherViewController: UIViewController{
+    
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -16,39 +18,81 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var searchTextField: UITextField!
     
     var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()//Popup
+        locationManager.requestLocation()
+        
+        
+        weatherManager.delegate = self
         searchTextField.delegate = self
     }
-
-    @IBAction func clickSearch(_ sender: UIButton) {
-        searchTextField.endEditing(true)
-        print(searchTextField.text!)
+    
+    @IBAction func clickLocation(_ sender: Any) {
+        locationManager.requestLocation()
     }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        searchTextField.endEditing(true)
-        return true
-    }
-    
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        if textField.text != ""{
-            return true
-        } else {
-            textField.placeholder = "Type something"
-            return false
-        }
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        //When textfield end editing and before resetting the textfield
-        if let city = searchTextField.text {
-            weatherManager.fetchWeather(cityName: city)
-        }
-        searchTextField.text = "" //If user done with editing searchTextField.endEditing(true)
-    }
-    
-    
 }
 
+
+    extension WeatherViewController: UITextFieldDelegate {
+        @IBAction func clickSearch(_ sender: UIButton) {
+            searchTextField.endEditing(true)
+            print(searchTextField.text!)
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            searchTextField.endEditing(true)
+            return true
+        }
+        
+        func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+            if textField.text != ""{
+                return true
+            } else {
+                textField.placeholder = "Type something"
+                return false
+            }
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            //When textfield end editing and before resetting the textfield
+            if let city = searchTextField.text {
+                weatherManager.fetchWeather(cityName: city)
+            }
+            searchTextField.text = "" //If user done with editing searchTextField.endEditing(true)
+        }
+    }
+
+    extension WeatherViewController: WeatherManagerDelegate {
+        func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+            DispatchQueue.main.async {
+                self.temperatureLabel.text = weather.temperatureString
+                self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+                self.cityLabel.text = weather.cityName
+            }
+        }
+        
+        func didFailWithError(error: Error) {
+            print(error)
+        }
+    }
+
+//Mark: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
--- WeatherManager.swift
+++ WeatherManager.swift
@@ -7,47 +7,75 @@
 //
 
 import Foundation
+import CoreLocation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
 
 struct WeatherManager {
     let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=10cf81edcba67b3b37fdb2a65108224b&units=metric"
     
+    var delegate: WeatherManagerDelegate?
+    
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherUrl)&q=\(cityName)"
-        performRequest(urlString: urlString)
+        performRequest(with: urlString)
     }
     
-    func performRequest(urlString: String) {
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
+    {
+        let urlString = "\(weatherUrl)&lat=\(latitude)&lon=\(longitude)"
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
         //Create a URL
         if let url = URL(string: urlString){
             
         //Create a URlsession
         let session = URLSession(configuration: .default)
-            
+                
         //Give the session a task
             let task = session.dataTask(with: url) { data, response, error in
                 if error != nil {
-                    print(error!)//Unwrap error
+                    self.delegate?.didFailWithError(error: error!)
                     return//If error happened exit out of this function
                 }
-                if let safeData = data {
-                    self.parseJSON(weatherData: safeData)//Inside the closure requires an explicit self if we are calling a method from the current class
-                }
+                    if let safeData = data {
+                        //weatherData
+                        //Inside the closure requires an explicit self if we are calling a method from the current class
+                        if let weather = self.parseJSON(safeData) {
+                            self.delegate?.didUpdateWeather(self, weather: weather)
+                        }
+                    }
             }
             //Start the task
             task.resume()
         }
     }
     
-    func parseJSON(weatherData: Data) {
+    //Here Weather Model need to be optional if there might be nil as a return
+    func parseJSON(_ weatherData: Data) -> WeatherModel? {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.main.temp)
-            print(decodedData.weather[0].description)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            return weather
+            
+            print(weather.conditionName)
+            print(weather.temperatureString)
         } catch {
-            print(error)
+            delegate?.didFailWithError(error: error)
+            return nil
         }
     }
+    
     
 //    func handle(data:Data?, response:URLResponse?, error:Error?) {
 //        //Check to see was there an error in this whole networking process
